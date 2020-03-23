13e5dced671acbaa528b7b67f0f9f7b8824b6b01
root <root@localhost.localdomain>
2020-03-23 15:52:59 +0200

    Lem-in

--- lem-in.c
+++ lem-in.c
@@ -1,5 +1,20 @@
 #include "lemin.h"
 
+int	ft_cmp(const char *s1, const char *s2)
+{
+	size_t l;
+
+	l = 0;
+	while (s1[l] && (unsigned char)s1[l] == (unsigned char)s2[l])
+		l++;
+	if ((unsigned char)s1[l] > (unsigned char)s2[l])
+		return (-1);
+	else if ((unsigned char)s1[l] < (unsigned char)s2[l])
+		return (1);
+	return (0);
+}
+
+
 static	size_t	ft_wordc(char const *s, int c)
 {
 	size_t	i;
@@ -69,11 +84,47 @@ char *room(char **str)
  return(rooms);
 }
 
+char *end(char **str)
+{
+ char *start;
+ char *end;
+ char *point;
+ int i; 
+ int c;
+
+ i = 0;
+   start = ft_strnew(0);
+   end = ft_strnew(0);
+ while(str[i] != NULL)
+ {
+   c = 0;
+   if (ft_strstr(str[i],"##start") != NULL)
+   {
+     ft_strdel(&start);
+     while (str[i + 1][c] != ' ' && str[i + 1][c] != '\0')
+     c++;
+     start = ft_strsub(str[i + 1], 0, c);
+   }
+   else if (ft_strstr(str[i],"##end") != NULL)
+   {
+     ft_strdel(&end);
+     while (str[i + 1][c] != ' ' && str[i + 1][c] != '\0')
+     c++;
+     end = ft_strsub(str[i + 1], 0, c);
+   }
+  i++;
+ }
+ point = ft_joint(start,"|",end);
+//   ft_strdel(&start);
+//   ft_strdel(&end);
+ return (point);
+}
+
 int main (void){
  char *s = rd();
  char **str = ft_strsplit(s, '\n');
  char *rooms = room(str);
- char *points = ends(str);
+ char *points = end(str);
  ft_putendl(rooms);
  ft_putendl(points);
  return (0);
